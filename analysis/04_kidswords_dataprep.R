dd28f7f2688a0548759286546aac1cb73147e07e
thomasklee <tom.klee@gmail.com>
2019-05-15 08:29:24 +0800

    object names cleaned up

--- analysis/04_kidswords_dataprep.R
+++ analysis/04_kidswords_dataprep.R
@@ -3,7 +3,7 @@
 # Data preparation-04
 # Thomas Klee
 # Created: 2019-05-14
-# Revised: 
+# Revised: 2019-05-15
 # ---------------------------
 
 # This script retrieves data from several sources
@@ -15,25 +15,26 @@ library(lubridate)
 
 # KidsWords.accdb was a MS Access database  
 # designed to keep records of those expressing 
-# interest in participating. 
+# interest in participating. That participants
+# database stored children's names.
 
 # load CSV files
-names <- read_csv("data/tb_Passwords.csv") # exported from KidsWords.accdb on 2019-05-14
+namedata <- read_csv("data/tb_Passwords.csv") # exported from KidsWords.accdb on 2019-05-14
 users <- read_delim("data/Kidswords_user_children_20161201.csv", delim = ";")
 
 # CSV file contains surnames and other confidential information, so
 # only select relevant variables
-names <- names %>% 
+namedata <- namedata %>% 
   select(Password_pwd, cFirstName_pwd, Email_date_pwd)
 
 # simplify variable names (new = old)
-names <- rename(names, 
+namedata <- rename(namedata, 
                 firstname = cFirstName_pwd,
                 emaildate = Email_date_pwd
 )
 
 # change dates to ISO 8601 format
-names$emaildate <- dmy(names$emaildate) # turns out we don't need this
+namedata$emaildate <- dmy(namedata$emaildate) # turns out we don't need this
 
 # we need these variables: 
 # users$childID # same as CDIPQ$PID
@@ -48,25 +49,28 @@ users <- users %>%
   mutate(Password_pwd = childReferenceID)
 
 # merge data frames
-test <- inner_join(users, names, by = "Password_pwd")
+names <- inner_join(users, namedata, by = "Password_pwd")
 
 # merge test and xs data frames to identify those who participated at Time 1
-test2 <- inner_join(test, xs, by = "PID")
-head(test2)
+names <- inner_join(names, xs, by = "PID")
 
 # select relevant variables for analysis
-test3 <- test2 %>% 
-  select(PID, firstname, csex)
-head(test3)
-str(test3)
+names <- names %>% 
+  select(firstname, csex) # PID not selected to keep things anonymous
+
+# display data frame structure
+str(names)
+
+# convert firstnames from characters to factor
+names$firstname <- as.factor(names$firstname)
+
+# display data frame structure and first 6 rows
+str(names)
+head(names)
 
 # create CSV file from data frame
-write_csv(test3, "data/data_test3.csv")
+write_csv(names, "data/data_names.csv")
 
 # remove temporary data frames
-rm(names)
-rm(names2)
+rm(namedata)
 rm(users)
-rm(test)
-rm(test2)
-rm(test3)
