ab7cce989e56054a9e8438d58496fe4cb5b9124f
thomasklee <tom.klee@gmail.com>
2019-12-12 14:29:27 +0800

    calculations corrected and verified

--- analysis/106_kidswords_stats.R
+++ analysis/106_kidswords_stats.R
@@ -11,7 +11,13 @@
 
 library(tidyverse)
 
-# calculate size of cross-sectional study 
+CDIPQ <- read.csv("data/data_CDIPQ.csv") 
+
+# select data for CDI normative study
+xs <- CDIPQ %>% 
+  filter(session == 1,  camos >= 16 & camos <= 30, mono_env == "yes")
+
+# calculate size of cross-sectional sample
 N <- (nrow(xs))
 
 # calculate current size of each education group
@@ -19,6 +25,12 @@ noqual_N <- nrow(filter(xs, pedcat == 0))
 somesec_N <-  nrow(filter(xs, pedcat == 1))
 sec_N <- nrow(filter(xs, pedcat == 2))
 univ_N <- nrow(filter(xs, pedcat == 3))
+
+# results
+noqual_N
+somesec_N
+sec_N
+univ_N
 
 # based on current sample size, 
 # calculate ideal size of each group in proportion to population 
@@ -28,28 +40,37 @@ ideal_somesec <- .246 * N
 ideal_sec <- .355 * N
 ideal_univ <- .271 * N
 
+# results (obviously cannot be achieved given current data set)
+ideal_noqual
+ideal_somesec
+ideal_sec
+ideal_univ
+
 # check N based on ideal group sizes 
 sizecheck <- ideal_noqual + ideal_somesec + ideal_sec + ideal_univ
+sizecheck
 
 # assuming no further data will be collected,
 # adjust size of each group relative to 
-# size of smallest current group (noqual: n = 40)
+# size of smallest current group (noqual: n = 40);
+# round to whole numbers
 
-# calculate adjustment values in relation to population
-somesec_val <- somesec_N / ideal_noqual
-sec_val <- sec_N / ideal_noqual
-univ_val <- univ_N / ideal_noqual
+adj_somesec <- round((40 * 24.6) / 12.7, 0)
+adj_sec <- round((40 * 35.5) / 12.7, 0)
+adj_univ <- round((40 * 27.1) / 12.7, 0)
 
-# calculate adjusted cell sizes
-adj_somesec <- somesec_val * noqual_N
-adj_sec <- sec_val * noqual_N
-adj_univ <- univ_val * noqual_N
+# results
+adj_somesec
+adj_sec
+adj_univ
 
-# calculate adjusted total sample size
+# total size of hypothetical adjusted sample
 adj_N <- noqual_N + adj_somesec + adj_sec + adj_univ
+adj_N
 
 # calculate reduction of ideal sample from current sample
 reduced_N <- round(1 - (adj_N / N), 2)
+reduced_N
 
 # clean up
 rm(N)
@@ -61,12 +82,8 @@ rm(adj_N)
 rm(adj_sec)
 rm(adj_somesec)
 rm(adj_univ)
-rm(sec_val)
-rm(somesec_val)
-rm(univ_val)
 rm(sizecheck)
 rm(reduced_N)
-rm(somesec)
 rm(noqual_N)
 rm(sec_N)
 rm(somesec_N)
